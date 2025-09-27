4d45323c298a3dc8f9bebac45e0ce5028d70e0d5
Mieczyslaw Nalewaj <namiltd@yahoo.com>
2025-09-27 08:07:22 +0200

    ramips: mt7620: set vermagic
    Set vermagic for version 24.10
    
    Signed-off-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -127,7 +127,8 @@ define Kernel/Configure/Default
 		cp $(LINUX_DIR)/.config.set $(LINUX_DIR)/.config.prev; \
 	}
 	$(_SINGLE) [ -d $(LINUX_DIR)/user_headers ] || $(KERNEL_MAKE) $(if $(findstring uml,$(BOARD)),ARCH=$(ARCH)) INSTALL_HDR_PATH=$(LINUX_DIR)/user_headers headers_install
-	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic
+#	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)/.vermagic
+	echo "8fc1468db1545d48bb03e8d3cf86e2d6" > $(LINUX_DIR)/.vermagic
 endef
 
 define Kernel/Configure/Initramfs
