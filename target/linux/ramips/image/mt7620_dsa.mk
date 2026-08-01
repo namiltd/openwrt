3b89a67be069b7f776281634f6c04055ea55db8d
Mieczyslaw Nalewaj <namiltd@yahoo.com>
2026-08-02 01:37:52 +0200

    ramips: add support for Mercusys AC12G v1 (8M) (DSA)
    Mercusys AC12G v1 can be supported by OpenWRT if we replace the flash memory installed in it from 2MB to 8MB (e.g. Winbond W25Q64FVSIG) to which we upload the original image from the TP-Link Archer C5 v4 router but with a few corrections. For this you will need a memory programmer, a soldering iron and a TP-Link Archer C5 v4 memory dump.
    
    You should carefully desolder the memory and then read it with a programmer. The read dump should be copied to the Archer C5 v4 dump in the following areas:
    from addresses 1FE000-1FE005 to 7DF100-7DF105
    from addresses 1FF000-1FF7FF to 7F0000-7F07FF
    from addresses 1FF800-1FFFFF to 7F8000-7F87FF
    The corrected image should be programmed into 8MB flash memory and soldered.
    
    Once your router is up and running, update it to the latest version from the TP-Link website.
    
    The latest version has a TFTP recovery function, so you can easily change the firmware to OpenWRT:
    -rename openwrt-ramips-mt7620-mercusys_ac12g-v1-8m-squashfs-tftp-recovery.bin to tp_recovery.bin
    -change the IP address of the computer to 192.168.0.66, connect to the LAN port of the router.
    -start the TFTP server
    -restart the router with the reset button pressed, the file will be automatically downloaded and after a while the router will restart. After updating, set your computer's IP address to DHCP.
    
    Signed-off-by: Mieczyslaw Nalewaj <namiltd@yahoo.com>

--- target/linux/ramips/image/mt7620_dsa.mk
+++ target/linux/ramips/image/mt7620_dsa.mk
@@ -4,3 +4,20 @@
 
 include ./common-sercomm.mk
 include ./common-tp-link.mk
+
+define Device/mercusys_ac12g-v1-8m-dsa
+  $(Device/tplink-v2)
+  SOC := mt7620a
+  IMAGE_SIZE := 7808k
+  TPLINK_FLASHLAYOUT := 8Mmtk
+  TPLINK_HWID := 0x04da857c
+  TPLINK_HWREV := 0x0c000600
+  TPLINK_HWREVADD := 0x04000000
+  IMAGES += tftp-recovery.bin
+  IMAGE/tftp-recovery.bin := pad-extra 128k | $$(IMAGE/factory.bin)
+  DEVICE_VENDOR := Mercusys
+  DEVICE_MODEL := AC12G
+  DEVICE_VARIANT := v1 (8M) (DSA)
+  DEVICE_PACKAGES := kmod-mt76x2 kmod-dsa-rtl8365mb kmod-fixed-phy kmod-ledtrig-network
+endef
+TARGET_DEVICES += mercusys_ac12g-v1-8m-dsa
